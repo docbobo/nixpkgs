3b80d11d106607f1827f49a0e4e4ec48c15aa925
Boris Pruessmann <boris@pruessmann.org>
2025-01-08 16:05:10 +0100

    igir: add darwin as supported platform

--- pkgs/by-name/ig/igir/package.nix
+++ pkgs/by-name/ig/igir/package.nix
@@ -1,13 +1,6 @@
-{
-  lib,
-  buildNpmPackage,
-  fetchFromGitHub,
+{ lib, buildNpmPackage, fetchFromGitHub,
 
-  # for patching bundled 7z binary from the 7zip-bin node module
-  # at lib/node_modules/igir/node_modules/7zip-bin/linux/x64/7za
-  autoPatchelfHook,
-  stdenv,
-}:
+stdenv, }:
 
 buildNpmPackage rec {
   pname = "igir";
@@ -20,28 +13,24 @@ buildNpmPackage rec {
     hash = "sha256-QPUnvirufY3UiSFVxX3xCmpAlzPKg5JxwAnVefdepqU=";
   };
 
-  npmDepsHash = "sha256-ADIEzr6PkGaJz27GKSVyTsrbz5zbud7BUb+OXPtP1Vo=";
+  npmDepsHash = "sha256-hJzgFWUJnEQu+EGD4itv04IQl7Rwm8CYeFELJ9aoTMY=";
 
   # I have no clue why I have to do this
   postPatch = ''
     patchShebangs scripts/update-readme-help.sh
   '';
 
-  nativeBuildInputs = [ autoPatchelfHook ];
-
+  dontPatchELF = true;
   buildInputs = [ (lib.getLib stdenv.cc.cc) ];
 
-  # from lib/node_modules/igir/node_modules/@node-rs/crc32-linux-x64-musl/crc32.linux-x64-musl.node
-  # Irrelevant to our use
-  autoPatchelfIgnoreMissingDeps = [ "libc.musl-x86_64.so.1" ];
-
   meta = with lib; {
-    description = "Video game ROM collection manager to help filter, sort, patch, archive, and report on collections on any OS";
+    description =
+      "Video game ROM collection manager to help filter, sort, patch, archive, and report on collections on any OS";
     mainProgram = "igir";
     homepage = "https://igir.io";
     changelog = "https://github.com/emmercm/igir/releases/tag/${src.rev}";
     license = licenses.gpl3Plus;
     maintainers = with maintainers; [ ];
-    platforms = platforms.linux;
+    platforms = platforms.linux ++ platforms.darwin;
   };
 }
