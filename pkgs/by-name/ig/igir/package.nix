6889238a08f10859e1ce5c63c653575574fb25af
Boris Pruessmann <boris@pruessmann.org>
2025-01-10 10:44:11 +0100

    igir: fixed maxcso library paths

--- pkgs/by-name/ig/igir/package.nix
+++ pkgs/by-name/ig/igir/package.nix
@@ -7,6 +7,10 @@
   SDL2,
   libuv,
   lz4,
+  #tests
+  runCommand,
+  igir,
+  nodejs,
 }:
 buildNpmPackage rec {
   pname = "igir";
@@ -19,29 +23,58 @@ buildNpmPackage rec {
     hash = "sha256-QPUnvirufY3UiSFVxX3xCmpAlzPKg5JxwAnVefdepqU=";
   };
 
-  npmDepsHash = "sha256-hJzgFWUJnEQu+EGD4itv04IQl7Rwm8CYeFELJ9aoTMY=";
+  npmDepsHash = "sha256-miXxMBtIqgxS4Ulhkq79VDCkBjDjoB8juPoDWE1UQUM=";
+
+  # This patch is only needed for 3.0.1 and needs to be removed afterwards
+  # along with the patch file itself
+  patches = [ ./0001-update-dependencies.patch ];
 
   # I have no clue why I have to do this
   postPatch = ''
     patchShebangs scripts/update-readme-help.sh
   '';
 
-  buildInputs =
-    [
-      (lib.getLib stdenv.cc.cc)
-      SDL2
-    ]
-    ++ lib.optionals stdenv.hostPlatform.isLinux [
-      libuv
-      lz4
-    ];
+  buildInputs = [
+    (lib.getLib stdenv.cc.cc)
+    SDL2
+    libuv
+    lz4
+  ];
 
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
 
   postFixup = lib.optionalString stdenv.hostPlatform.isDarwin ''
+    # Fix chdman
     find $out/lib/node_modules/igir/node_modules/@emmercm/ -name chdman -executable -type f \
       -exec install_name_tool -change /opt/homebrew/opt/sdl2/lib/libSDL2-2.0.0.dylib ${SDL2}/lib/libSDL2-2.0.0.dylib {} \;
+
+    # Fix maxcso
+    find $out/lib/node_modules/igir/node_modules/@emmercm/ -name maxcso -executable -type f \
+      -exec install_name_tool -change /opt/homebrew/opt/libuv/lib/libuv.1.dylib ${libuv}/lib/libuv.1.dylib {} \;
+    find $out/lib/node_modules/igir/node_modules/@emmercm/ -name maxcso -executable -type f \
+      -exec install_name_tool -change /opt/homebrew/opt/lz4/lib/liblz4.1.dylib ${lz4.lib}/lib/liblz4.1.dylib {} \;
   '';
+
+  passthru.tests =
+    let
+      igirDir = "${igir}/lib/node_modules/igir";
+      npxCmd = "${nodejs}/bin/npx";
+    in
+    {
+      chdman = runCommand "${pname}-chdman" { meta.timeout = 30; } ''
+        set +e
+        cd ${igirDir}
+        ${npxCmd} chdman > $out
+        grep -q "chdman - MAME Compressed Hunks of Data (CHD) manager" $out
+      '';
+
+      maxcso = runCommand "${pname}-maxcso" { meta.timeout = 30; } ''
+        set +e
+        cd ${igirDir}
+        ${npxCmd} maxcso 2> $out
+        grep -q "maxcso v1.13.0" $out
+      '';
+    };
 
   meta = {
     description = "Video game ROM collection manager to help filter, sort, patch, archive, and report on collections on any OS";
